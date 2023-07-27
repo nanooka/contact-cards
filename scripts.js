1f6f14164acf3d8760a85b731a2694adc1e040c5
nanooka <102428722+nanooka@users.noreply.github.com>
2023-07-27 21:22:10 +0400

    last

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 let cardCounter = 1;
+
 const addBtn = document.querySelector(".add-btn");
 const submit = document.getElementById("submit");
 // const info = document.getElementById("info");
 const info = document.querySelector(".formDiv");
 const container = document.querySelector(".container");
+
 const form = document
   .getElementById("newFormSomething")
   .addEventListener("submit", (event) => {
@@ -17,8 +19,6 @@ const form = document
 
     let newCardDiv = document.createElement("div");
     newCardDiv.classList.add("card");
-    // let text = document.createTextNode("hello world");
-    // newCardDiv.appendChild(text);
     newCardDiv.innerHTML = `
     <img class="photo" src="https://img.freepik.com/premium-vector/young-girl-anime-style-character-vector-illustration-design-manga-anime-girl-hair-faces-cartoon_147933-1139.jpg" alt="photo">
     <div class="contact">
@@ -31,48 +31,21 @@ const form = document
           <button class="delete">Delete</button>
         </div>
     `;
+
     container.appendChild(newCardDiv);
+
+    const deleteBtn = document.querySelectorAll(".delete");
+    deleteBtn.forEach((button) => {
+      button.addEventListener("click", () => {
+        console.log("????????", this.parentNode);
+      });
+    });
   });
 
 function givi() {
   console.log("clicked submit");
   return form;
-  // let tag = document.createElement("p");
-  // let text = document.createTextNode("hello world");
-  // tag.appendChild(text);
-  // container.appendChild(tag);
 }
-
-// submit.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   // let tag = document.createElement("p");
-//   // let text = document.createTextNode("hello world");
-//   // tag.appendChild(text);
-//   // container.appendChild(tag);
-
-//   // let newCardDiv = document.createElement("div");
-//   // newCardDiv.classList.add("card");
-
-//   // let text = (newCardDiv.innerHTML = `
-//   // <img
-//   //         class="photo"
-//   //         src="${cards.img}"
-//   //         alt="photo"
-//   //       />
-//   //       <div class="contact">
-//   //         <h2>${cards.name}</h2>
-//   //         <p>${cards.phone}</p>
-//   //         <p>${cards.email}</p>
-//   //       </div>
-//   //       <div class="del-edit">
-//   //         <button class="edit">Edit</button>
-//   //         <button class="delete">Delete</button>
-//   //       </div>
-//   // `);
-
-//   // newCardDiv.appendChild(text);
-//   // container.appendChild(newCardDiv);
-// });
 
 /*
 
@@ -107,8 +80,6 @@ const cards = [
   },
 ];
 
-const postContainer = document.querySelector(".container");
-
 const postMethods = () => {
   cards.map((postData) => {
     const postElement = document.createElement("div");
@@ -129,32 +100,10 @@ const postMethods = () => {
           <button class="delete">Delete</button>
         </div>
     `;
-    postContainer.appendChild(postElement);
+    container.appendChild(postElement);
   });
 };
 postMethods();
-
-// function addContactCard() {
-//   const contactContainer = document.getElementById("card");
-//   addBtn.classList.add("hide");
-//   const newCardHTML = `
-//     <form class="info">
-//       <label for="name">Name</label>
-//       <input type="text" name="name"  id="name" autocomplete="given-name"/>
-
-//       <label for="number">Phone number</label>
-//       <input type="text" name="number"  id="number"/>
-
-//       <label for="email">Email</label>
-//       <input type="email" name="email"  id="email" autocomplete="off"/>
-
-//       <button id="submit">Submit</button>
-//   </form>
-//     `;
-
-//   contactContainer.insertAdjacentElement("beforeend", newCardHTML);
-//   cardCounter++;
-// }
 
 function gikeqsa(param1) {
   let pp = {
@@ -171,7 +120,7 @@ function gikeqsa(param1) {
 addBtn.addEventListener("click", (event) => {
   event.preventDefault();
   addBtn.classList.add("hide");
-  // info.classList.add("visible");
+  // ??????????????????????????
   // info.classList.add("visible");
   // form.classList.add("visible");
 });
