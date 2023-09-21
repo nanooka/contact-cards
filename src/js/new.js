97181e28a0509941e140b4f56da17e6d1520a92e
Rostomi <rostomi1306@gmail.com>
2023-09-21 16:51:51 +0400

    add axios request

--- src/js/new.js
+++ src/js/new.js
@@ -4,6 +4,7 @@ const addNewContact = document.getElementById("add-modal");
 const cancelButton = addNewContact.querySelector(".cancel");
 const addButton = addNewContact.querySelector(".add");
 const userInputs = addNewContact.querySelectorAll("input");
+const axios = require("axios");
 
 const contacts = [];
 
@@ -37,114 +38,121 @@ const cancelAddContactHandler = () => {
   clearContactInput();
 };
 
-const editing = (contactId) => {
-  let contactIndex = 0;
-  for (const contact of contacts) {
-    if (contact.id === contactId) {
-      break;
-    }
-    contactIndex++;
+const nameValue = userInputs[0].value;
+const numberValue = userInputs[1].value;
+const emailValue = userInputs[2].value;
+const imageUrlValue = userInputs[3].value;
+
+if (nameValue.trim() === "" || numberValue.trim() === "") {
+  alert("Please fill the gaps");
+}
+
+const newContact = {
+  name: nameValue,
+  number: numberValue,
+  email: emailValue,
+  image: imageUrlValue,
+};
+
+async function addContactHandler() {
+  try {
+    const result = await axios.get("http://localhost:3001/contacts");
+    console.log("es aris mongodan wamosuli data ", result.data);
+  } catch (error) {
+    console.log(error);
   }
-  contacts.splice(contactIndex, 1);
-  const listRoot = document.getElementById("contact-list");
-  // listRoot.children[contactIndex].remove();
+}
 
-  const email = document.getElementById("email");
-  showContactModal();
-  userInputs[0].value =
-    listRoot.children[contactIndex].children[1].children[0].textContent;
+console.log("This Is New Contact", newContact);
 
-  userInputs[1].value =
-    listRoot.children[contactIndex].children[1].children[1].textContent;
+closeContactModal();
+clearContactInput();
 
-  userInputs[2].value =
-    listRoot.children[contactIndex].children[1].children[2].textContent;
-
-  const imageValue = document.querySelector(".contact-element__image");
-  userInputs[3].value = imageValue.getAttribute("src");
-
-  console.log(listRoot.children[contactIndex].innerHTML);
-  listRoot.children[contactIndex].remove();
-};
-
-const deleting = (contactId) => {
-  let contactIndex = 0;
-  for (const contact of contacts) {
-    if (contact.id === contactId) {
-      break;
-    }
-    contactIndex++;
-  }
-  contacts.splice(contactIndex, 1);
-  const listRoot = document.getElementById("contact-list");
-  listRoot.children[contactIndex].remove();
-};
-
-const renderNewContactElement = (id, name, number, email, image) => {
-  const newContactElement = document.createElement("li");
-  newContactElement.className = "contact-element";
-  newContactElement.innerHTML = `
-    <div>
-        <img src="${image}" alt="photo of ${name}" class="contact-element__image">
-    </div>
-    <div class="contact-element__info">
-        <h2>${name}</h2>
-        <p>${number}</p>
-        <p>${email}</p>
-    </div>
-    `;
-
-  const listRoot = document.getElementById("contact-list");
-  listRoot.append(newContactElement);
-
-  // edit
-  const editButton = document.createElement("button");
-  editButton.className = "btn edit-button";
-  editButton.textContent = "Edit";
-  editButton.addEventListener("click", editing.bind(null, id));
-  newContactElement.append(editButton);
-
-  // delete
-
-  const deleteButton = document.createElement("button");
-  deleteButton.className = "btn delete-button";
-  deleteButton.textContent = "Delete";
-  deleteButton.addEventListener("click", deleting.bind(null, id));
-  newContactElement.append(deleteButton);
-};
-
-const addContactHandler = () => {
-  const nameValue = userInputs[0].value;
-  const numberValue = userInputs[1].value;
-  const emailValue = userInputs[2].value;
-  const imageUrlValue = userInputs[3].value;
-
-  if (nameValue.trim() === "" || numberValue.trim() === "") {
-    alert("Please fill the gaps");
-  }
-
-  const newContact = {
-    id: Math.random().toString(),
-    name: nameValue,
-    number: numberValue,
-    email: emailValue,
-    image: imageUrlValue,
-  };
-
-  contacts.push(newContact);
-  console.log(contacts);
-  closeContactModal();
-  clearContactInput();
-  renderNewContactElement(
-    newContact.id,
-    newContact.name,
-    newContact.number,
-    newContact.email,
-    newContact.image,
-  );
-};
+// renderNewContactElement(
+//   newContact.id,
+//   newContact.name,
+//   newContact.number,
+//   newContact.email,
+//   newContact.image
+// );
 
 addContactButton.addEventListener("click", showContactModal);
 backdrop.addEventListener("click", backdropClickHandler);
 cancelButton.addEventListener("click", cancelAddContactHandler);
 addButton.addEventListener("click", addContactHandler);
+
+// const deleting = (contactId) => {
+//   let contactIndex = 0;
+//   for (const contact of contacts) {
+//     if (contact.id === contactId) {
+//       break;
+//     }
+//     contactIndex++;
+//   }
+//   contacts.splice(contactIndex, 1);
+//   const listRoot = document.getElementById("contact-list");
+//   listRoot.children[contactIndex].remove();
+// };
+
+// const renderNewContactElement = (id, name, number, email, image) => {
+//   const newContactElement = document.createElement("li");
+//   newContactElement.className = "contact-element";
+//   newContactElement.innerHTML = `
+//     <div>
+//         <img src="${image}" alt="photo of ${name}" class="contact-element__image">
+//     </div>
+//     <div class="contact-element__info">
+//         <h2>${name}</h2>
+//         <p>${number}</p>
+//         <p>${email}</p>
+//     </div>
+//     `;
+
+// const listRoot = document.getElementById("contact-list");
+// listRoot.append(newContactElement);
+
+// edit
+// const editButton = document.createElement("button");
+// editButton.className = "btn edit-button";
+// editButton.textContent = "Edit";
+// editButton.addEventListener("click", editing.bind(null, id));
+// newContactElement.append(editButton);
+
+// delete
+
+// const deleteButton = document.createElement("button");
+// deleteButton.className = "btn delete-button";
+// deleteButton.textContent = "Delete";
+// deleteButton.addEventListener("click", deleting.bind(null, id));
+// newContactElement.append(deleteButton);
+// };
+
+// const editing = (contactId) => {
+//   let contactIndex = 0;
+//   for (const contact of contacts) {
+//     if (contact.id === contactId) {
+//       break;
+//     }
+//     contactIndex++;
+//   }
+//   contacts.splice(contactIndex, 1);
+//   const listRoot = document.getElementById("contact-list");
+//   // listRoot.children[contactIndex].remove();
+
+//   const email = document.getElementById("email");
+//   showContactModal();
+//   userInputs[0].value =
+//     listRoot.children[contactIndex].children[1].children[0].textContent;
+
+//   userInputs[1].value =
+//     listRoot.children[contactIndex].children[1].children[1].textContent;
+
+//   userInputs[2].value =
+//     listRoot.children[contactIndex].children[1].children[2].textContent;
+
+//   const imageValue = document.querySelector(".contact-element__image");
+//   userInputs[3].value = imageValue.getAttribute("src");
+
+//   console.log(listRoot.children[contactIndex].innerHTML);
+//   listRoot.children[contactIndex].remove();
+// };
